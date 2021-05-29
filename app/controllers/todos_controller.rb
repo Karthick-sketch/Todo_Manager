41a4d232d3e6bca0305413c5e47e37604f86e0bc
Karthick-sketch <karthick246.kh@outlook.com>
2021-05-29 23:45:32 +0530

    Add Update method to change todo completed status

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,4 +18,12 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
     render plain: "Hey, you are new todo is created with the id #{new_todo.id}"
   end
+
+  def update
+    completed = params[:completed]
+    todo = Todo.find(params[:id])
+    todo.completed = completed
+    todo.save!
+    render plain: "Upload todo completed status to #{completed}"
+  end
 end
