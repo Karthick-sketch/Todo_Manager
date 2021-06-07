d0088cd7aa3ea22d960f09d9d4662ca984237cc1
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-07 22:23:50 +0530

    Add user sign-in functionality (without session)

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   post "users/login", to: "users#login"
   resources :users
+
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
