bbb344bb1e0f2e58cb56d0c6e3081c3c909b4f7d
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-08 21:42:14 +0530

    Modified todo model and removed unwanted files and codes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,19 @@
-# todos_controller.rb
 class TodosController < ApplicationController
   def index
-    # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
 
   def show
-    # todo = Todo.find(params[:id])
-    # render plain: todo.to_pleasant_string
     render "todo"
   end
 
   def create
-    todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
+    Todo.create!(
+      todo_text: params[:todo_text],
+      due_date: DateTime.parse(params[:due_date]),
+      completed: false,
+    )
 
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    # render plain: "Hey, you are new todo is created with the id #{new_todo.id}"
     redirect_to todos_path
   end
 
@@ -25,7 +22,7 @@ class TodosController < ApplicationController
     todo = Todo.find(params[:id])
     todo.completed = completed
     todo.save!
-    # render plain: "Upload todo completed status to #{completed}"
+
     redirect_to todos_path
   end
 
