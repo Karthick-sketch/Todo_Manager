44aa2cb31a0b805b9dc4a2073b303895f910eaa3
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-08 14:46:36 +0530

    Remove unwanted codes and files

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,22 +3,20 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
     render "index"
   end
 
   def show
-    # todo = Todo.find(params[:id])
-    # render plain: todo.to_pleasant_string
     render "todo"
   end
 
   def create
-    todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    render plain: "Hey, you are new todo is created with the id #{new_todo.id}"
+    Todo.create!(
+      todo_text: params[:todo_text],
+      due_date: DateTime.parse(params[:due_date]),
+      completed: false,
+    )
+    render plain: "Hey, you are new todo is created with the id #{Todo.last.id}"
   end
 
   def update
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,12 +1,7 @@
-# todo.rb
 class Todo < ActiveRecord::Base
-  def due_today?
-    due_date == Date.today
-  end
-
   def self.overdue
     # get to-dos from database that are overdue
-    where("due_date < ?", Date.today)
+    where("due_date < ? and (not completed)", Date.today).order(:due_date)
   end
 
   def self.due_today
@@ -21,44 +16,5 @@ class Todo < ActiveRecord::Base
 
   def self.completed
     where(completed: true)
-  end
-
-  def to_displayable_string
-    display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
-    "#{id}. #{display_status} #{todo_text} #{display_date}"
-  end
-
-  def self.to_displayable_list
-    all.map { |todo| todo.to_displayable_string }.join("\n")
-  end
-
-  def self.show_list
-    # print the list of todos
-    puts "My Todo-list\n\n"
-
-    puts "Overdue\n"
-    puts overdue.to_displayable_list
-    puts "\n\n"
-
-    puts "Due Today\n"
-    puts due_today.to_displayable_list
-    puts "\n\n"
-
-    puts "Due Later\n"
-    puts due_later.to_displayable_list
-    puts "\n\n"
-  end
-
-  def self.add_task(hash)
-    # take a Ruby hash as parameter, containing todo_text and due_in_days.
-    Todo.create!(todo_text: hash[:todo_text], due_date: Date.today + hash[:due_in_days], completed: false)
-  end
-
-  def self.mark_as_complete(id)
-    todo = Todo.find(id)
-    # set its completed to true
-    todo.completed = true; todo.save!
-    todo
   end
 end
