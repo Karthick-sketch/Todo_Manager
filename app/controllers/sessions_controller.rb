a42b2fbf1fe29c49709d4b9b03df0534d3a6b4b0
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-09 22:57:55 +0530

    Added signing-in, updating session and fenching off protected routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
+    session[:current_user_id] = user.id
     if user and user.authenticate(params[:password])
-      render plain: "Welcome " + user.first_name
+      redirect_to todos_path
     else
       render plain: "Invalid password!"
     end
