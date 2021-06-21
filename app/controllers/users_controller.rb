7b9a56044666c6022dd6a09c4928aa4e9e0961f5
Karthick-sketch <karthick246.kh@outlook.com>
2021-06-21 15:12:52 +0530

    Validate email with help of ActiveRecord validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,25 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    email = params[:email]
-    unless User.is_the_email_exist?(email)
-      user = User.new(
-        first_name: params[:first_name],
-        last_name: params[:last_name],
-        email: email,
-        password: params[:password],
-      )
+    user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+    )
 
-      unless user.save
-        flash[:error] = user.errors.full_messages.join(", ")
-        redirect_to new_user_path
-      else
-        session[:current_user_id] = user.id
-        redirect_to "/"
-      end
-    else
-      flash[:error] = "This email is already taken by an user"
+    unless user.save
+      flash[:error] = user.errors.full_messages.join(", ")
       redirect_to new_user_path
+    else
+      session[:current_user_id] = user.id
+      redirect_to "/"
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
-  validates :first_name, presence: true
-  validates :first_name, length: { minimum: 2 }
-  validates :email, presence: true
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true
 
   has_secure_password
   has_many :todos
-
-  def self.is_the_email_exist?(email)
-    User.where(email: email).present?
-  end
 end
